c6ffafb9cf1a7a6f9b8f187d2d8189880a718367
lukas303 <lukasfh94@gmail.com>
2024-01-21 10:05:11 +0100

    Removed old gallery

--- scripts.js
+++ scripts.js
@@ -1,41 +1,3 @@
-const images = [
-  "image1.png",
-  "image4.png",
-];
-
-let currentIndex = 0;
-
-function showImage(index) {
-  const img = document.querySelector("#image");
-  img.src = "images/" + images[index];
-  currentIndex = index;
-}
-
-function updateButtons() {
-  const previousButton = document.querySelector("#previous");
-  const nextButton = document.querySelector("#next");
-  previousButton.disabled = currentIndex === 0;
-  nextButton.disabled = currentIndex === images.length - 1;
-  previousButton.style.display = previousButton.disabled ? "none" : "inline-block";
-  nextButton.style.display = nextButton.disabled ? "none" : "inline-block";
-}
-
-function nextImage() {
-if (currentIndex < images.length - 1) {
-  currentIndex++;
-  showImage(currentIndex);
-  updateButtons();
-}
-}
-
-function previousImage() {
-if (currentIndex > 0) {
-  currentIndex--;
-  showImage(currentIndex);
-  updateButtons();
-}
-}
-
 function toggleInfo() {
   var x = document.getElementById("info");
   if (x.style.display === "none") {
@@ -44,9 +6,3 @@ function toggleInfo() {
     x.style.display = "none";
   }
 }
-
-showImage(currentIndex);
-updateButtons();
-
-document.querySelector("#previous").addEventListener("click", previousImage);
-document.querySelector("#next").addEventListener("click", nextImage);
