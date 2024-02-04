95107f6b02c02f1e1c7b2f83f4dff151f0392d88
lukas303 <lukasfh94@gmail.com>
2024-02-04 20:25:47 +0100

    Updated mobile image display size

--- scripts.js
+++ scripts.js
@@ -48,10 +48,10 @@ document.addEventListener("DOMContentLoaded", function () {
       var imageWidth, imageHeight;
 
       if (visibleWidth < 400) {
-          randomImage.style.width = "170px";
-          randomImage.style.height = "255px";
-          imageWidth = 170;
-          imageHeight = 255;
+          randomImage.style.width = "402px";
+          randomImage.style.height = "708px";
+          imageWidth = 272;
+          imageHeight = 408;
       } else {
           randomImage.style.width = "";
           randomImage.style.height = "";
