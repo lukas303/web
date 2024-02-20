f9e2747ffda8fbd6fd6f2a1c1e48215935793a7c
lukas303 <lukasfh94@gmail.com>
2024-02-20 23:51:49 +0100

    Updated titles

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var currentImageIndex = 0;
 var images = ["images/image1.png", "images/image2.png", "images/image3.png"]; // Array of image paths
-var labels = ["Bookself Speaker, Berlin 2023", "Burned Pipe, Ireland 2018", "Photo Booth, Leipzig 2022"]; // Array of corresponding texts
+var labels = ["2023", "2021", "2022"]; // Array of corresponding texts
 
 function changeImage() {
     var image = document.getElementById('image');
