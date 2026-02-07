c9b9a333f42c06ef175a748c7da5b5864be8162f
lukas303 <lukasfh94@gmail.com>
2026-02-07 14:34:30 +0100

    Added phase8 and added image auto change

--- scripts.js
+++ scripts.js
@@ -1,12 +1,33 @@
 var currentImageIndex = 0;
-var images = ["images/image2.jpg", "images/image3.jpg","images/image4.jpg", "images/image5.jpg"]; // Array of image paths
-var labels = ["Bookshelf Speakers, 2022", "Acoustic Synthesis_phase5, 2023", "Library Speakers, 2023", "Acoustic Synthesis_phase8, 2024"]; // Array of corresponding texts
+
+var images = ["images/image2.jpg", 
+    "images/image3.jpg",
+    "images/image4.jpg",
+    "images/image5.jpg",
+    "images/image6.jpg"
+]; // Array of image paths
+
+var labels = ["Bookshelf Speakers, 2022", 
+    "Acoustic Synthesis_phase5, 2023", 
+    "Library Speakers, 2023", 
+    "Acoustic Synthesis_phase8, 2024", 
+    "phase8, 2025"
+]; // Array of corresponding texts
+
 
 function changeImage() {
     var image = document.getElementById('image');
     var label = document.getElementById('label');
 
-    currentImageIndex = (currentImageIndex + 1) % images.length; // Cycle through images
-    image.src = images[currentImageIndex]; // Change image source
-    label.textContent = labels[currentImageIndex]; // Change text content
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    image.src = images[currentImageIndex];
+    label.textContent = labels[currentImageIndex];
 }
+
+var interval = setInterval(changeImage, 8000);
+
+var img = document.getElementById('image');
+img.addEventListener('mouseenter', () => clearInterval(interval));
+img.addEventListener('mouseleave', () => {
+    interval = setInterval(changeImage, 8000);
+});
